d8d2576f14152ab9f2d2c3a91416669991656701
mshore-dev <iam@mshore.dev>
2024-08-06 21:52:59 -0400

    add initial youtube impl

---
+++ youtube.go
@@ -0,0 +1,48 @@
+package metadaemon
+
+import (
+	"context"
+	"encoding/json"
+	"os/exec"
+	"time"
+)
+
+type youtubeTrackMetadata struct {
+	Track     string   `json:"track"`
+	Thumbnail string   `json:"thumbnail"`
+	Album     string   `json:"album"`
+	Artists   []string `json:"artists"`
+	URL       string   `json:"original_url"`
+}
+
+func (m *MetaDaemon) GetYouTubeMetadata(id string) (*ParsedMetadata, error) {
+
+	// https://music.youtube.com/watch?v=:id
+
+	// TODO: don't hardcode timeout
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	cmd := exec.CommandContext(ctx, "yt-dlp", "-j", "https://music.youtube.com/watch?v="+id)
+
+	out, err := cmd.Output()
+	if err != nil {
+		return &ParsedMetadata{}, err
+	}
+
+	var meta youtubeTrackMetadata
+
+	err = json.Unmarshal(out, &meta)
+	if err != nil {
+		return &ParsedMetadata{}, err
+	}
+
+	return &ParsedMetadata{
+		URL:       meta.URL,
+		Artists:   meta.Artists,
+		Name:      meta.Track,
+		Album:     meta.Album,
+		Thumbnail: meta.Thumbnail,
+	}, nil
+
+}
---
+++ youtube_test.go
@@ -0,0 +1,26 @@
+package metadaemon
+
+import "testing"
+
+func TestYouTubeGetMetadata(t *testing.T) {
+
+	meta, err := md.GetYouTubeMetadata("uQTBzmBDSv0")
+	if err != nil {
+		t.Fatalf("failed to get track metadata: %v\n", err)
+	}
+
+	if meta.Name != "The Town Inside Me" {
+		t.Fatalf("fetched track name did not match expected")
+	}
+
+	if meta.URL != "https://music.youtube.com/watch?v=uQTBzmBDSv0" {
+		t.Fatalf("fetched track url did not match expected")
+	}
+
+	if meta.Artists[0] != "AISHA" {
+		t.Fatal("fetched artist 0 did not match expected")
+	}
+
+	t.Logf("%v\n", meta)
+
+}
